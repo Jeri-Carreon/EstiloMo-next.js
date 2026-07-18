c572542a6dfde4f703e6d8615c1d3e3ac65d9e83
Jeri Carreon <jericarreon51@gmail.com>
2026-07-18 21:48:41 +0800

    revised corsheaders

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -50,22 +50,10 @@ const securityHeaders = [
 ];
 
 const corsHeaders = [
-  {
-    key: "Access-Control-Allow-Origin",
-    value: appUrl,
-  },
-  {
-    key: "Access-Control-Allow-Methods",
-    value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-  },
-  {
-    key: "Access-Control-Allow-Headers",
-    value: "Content-Type, Authorization",
-  },
-  {
-    key: "Access-Control-Allow-Credentials",
-    value: "true",
-  },
+  { key: "Access-Control-Allow-Origin", value: appUrl },
+  { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, PATCH, DELETE, OPTIONS" },
+  { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+  { key: "Access-Control-Allow-Credentials", value: "true" },
 ];
 
 const nextConfig: NextConfig = {
